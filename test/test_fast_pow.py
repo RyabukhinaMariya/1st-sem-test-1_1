1bf9d8cdcc84c46db165e55653a03850fa01cba0
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-10-27 16:34:49 +0300

    Update test_fast_pow.py
    Добавлены тесты для отрицательных и нулевой степени

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -7,3 +7,11 @@ def test_two_power_two():
 
 def test_negative():
     assert fastPow(-1, 4) == 1
+
+
+def test neg_pow():
+    assert fastPow(2, -1) == 0.5
+
+
+def test zero_pow():
+    assert fastPow(2, 0) == 1
