5aca7fbf400b9673c37e69eddb04b1462d36b765
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-10-27 16:37:56 +0300

    Update test_luhn.py
    Добавлены тесты для пустого номера

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -7,3 +7,11 @@ def test_good():
 
 def test_bad():
     assert not luhnСheck("4561 2612 1234 5463")
+
+
+def test_empty_num():
+    assert not luhnСheck("") == "Error"
+
+
+def test_empty_num():
+    assert not luhnСheck("qwerty") == "Error"
