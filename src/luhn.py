df3172de2983671567558b68b4ed7d0a2b22cb23
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-10-27 16:15:10 +0300

    Update luhn.py
    Добавлено исправление для пустого номера/ номера состоящего только из символов и ввод номеров из консоли

--- src/luhn.py
+++ src/luhn.py
@@ -1,5 +1,7 @@
 def luhnСheck(cardNumber):
     digits = [int(d) for d in str(cardNumber) if d.isdigit()]
+    if len(digits) == 0:
+        return 'Error'
     control = digits.pop()
     parity = (len(digits))%2
     total = 0
@@ -12,3 +14,16 @@ def luhnСheck(cardNumber):
         else:
             total += digits[i]
     return (total + control) % 10 == 0
+
+while (1):
+    n = input()
+    if n == '-1':
+        quit()
+    result = luhnСheck(n)
+    if result != 'Error':
+        if result:
+            print('correct')
+        else:
+            print('incorrect')
+    else:
+        print(result)
